92687850ec1da22c13c8782773eaae1931c1bf1d
KSuvery <luonglkse171576@fpt.edu.vn>
2025-03-19 16:30:17 +0700

    fix

---
+++ src/PRM_ProductSale_G5/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.ApiModels;
+using Services.ApiModels.PaginatedList;
+using Services.ApiModels.Product;
+using Services.Constants;
+using Services.Services;
+using System;
+
+namespace PRM_ProductSale_G5.Controllers
+{
+    [ApiController]
+    public class ProductController(IServiceProvider serviceProvider) : ControllerBase
+    {
+        private readonly ProductService _productService = serviceProvider.GetRequiredService<ProductService>();
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,Customer")]
+        [Route(WebApiEndpoint.Product.GetProducts)]
+        public async Task<IActionResult> GetProducts([FromQuery] PaginatedListRequest request)
+        {
+            return Ok(BaseResponse.OkResponseDto(
+                await _productService.GetAllProductsAsync(request.PageNumber, request.PageSize)));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,Customer")]
+        [Route(WebApiEndpoint.Product.GetProduct)]
+        public async Task<IActionResult> GetProduct([FromRoute] int id)
+        {
+            return Ok(BaseResponse.OkResponseDto(await _productService.GetProductByIdAsync(id)));
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "Admin")]
+        [Route(WebApiEndpoint.Product.UpdateProduct)]
+        public async Task<IActionResult> UpdateProduct([FromBody] ProductUpdateRequest request)
+        {
+            return Ok(BaseResponse.OkResponseDto(await _productService.UpdateProductAsync(request)));
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "Admin")]
+        [Route(WebApiEndpoint.Product.DeleteProduct)]
+        public async Task<IActionResult> DeleteProduct([FromRoute] int id)
+        {
+            return Ok(BaseResponse.OkResponseDto(await _productService.DeleteProductAsync(id)));
+        }
+    }
+}
---
+++ src/Services/Services/ProductService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Repositories.Models;
+using Repositories.Repositories;
+using Services.ApiModels.PaginatedList;
+using Services.ApiModels.Product;
+using Services.Constants;
+using Services.Exceptions;
+using Services.Mapper;
+using Serilog;
+using Microsoft.AspNetCore.Http;
+
+namespace Services.Services
+{
+    public interface IProductService
+    {
+        Task<PaginatedListResponse<ProductResponse>> GetAllProductsAsync(int pageNumber, int pageSize);
+        Task<ProductResponse> GetProductByIdAsync(int id);
+        Task<int> UpdateProductAsync(ProductUpdateRequest request);
+        Task<int> DeleteProductAsync(int id);
+    }
+
+    public class ProductService(IServiceProvider serviceProvider) : IProductService
+    {
+        private readonly ProductRepository _productRepository = serviceProvider.GetRequiredService<ProductRepository>();
+        private readonly ILogger _logger = serviceProvider.GetRequiredService<ILogger>();
+        private readonly MapperlyMapper _mapper = serviceProvider.GetRequiredService<MapperlyMapper>();
+
+        public async Task<PaginatedListResponse<ProductResponse>> GetAllProductsAsync(int pageNumber, int pageSize)
+        {
+            _logger.Information($"Getting {pageSize} products at page {pageNumber}");
+            var products = await _productRepository.GetAllPaginatedQueryable(pageNumber, pageSize);
+            if (products is null)
+            {
+                throw new AppException(ResponseCodeConstants.BAD_REQUEST,
+                    ResponseMessageConstrantsProduct.NOTFOUND, StatusCodes.Status400BadRequest);
+            }
+
+            return _mapper.Map(products);
+        }
+
+        public async Task<ProductResponse> GetProductByIdAsync(int id)
+        {
+            _logger.Information($"Getting product with id {id}");
+            var product = await _productRepository.GetSingleAsync(x => x.ProductId == id);
+            return _mapper.Map(product);
+        }
+
+        public async Task<int> UpdateProductAsync(ProductUpdateRequest request)
+        {
+            _logger.Information("Updating product {request}", request.ProductId);
+            var product = await GetProductById(request.ProductId);
+
+            _mapper.Map(request, product);
+            _productRepository.Update(product);
+            return await _productRepository.SaveChangeAsync();
+        }
+
+        public async Task<int> DeleteProductAsync(int id)
+        {
+            _logger.Information("Deleting product with id {id}", id);
+            var product = await GetProductById(id);
+            _productRepository.Remove(product);
+            return await _productRepository.SaveChangeAsync();
+        }
+
+        private async Task<Product> GetProductById(int id)
+        {
+            var product = await _productRepository.GetSingleAsync(x => x.ProductId == id);
+            if (product is null)
+            {
+                throw new AppException(ResponseCodeConstants.BAD_REQUEST,
+                    ResponseMessageConstrantsProduct.NOTFOUND, StatusCodes.Status400BadRequest);
+            }
+
+            return product;
+        }
+    }
+}
